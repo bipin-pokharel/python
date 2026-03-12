662a6a9f912af958cf3c16885ff38d194c8c09ca
bipin pokharel <pokharelbipin850@gmail.com>
2026-03-12 14:23:26 +0545

    lists

--- lists.py
+++ lists.py
@@ -1,7 +1,53 @@
-users = ["Dave", "john", "sara"]
+users = ["Dave", "John", "Sara"]
 
 data = ["Dave", 42, True]
 
 emptylist = []
 
-print("dave" in users)
+print("Dave" in users)
+print(users[0])
+print(users[-1])
+print(users[-2])
+
+print(users.index("Sara"))
+print(users[0:])
+
+print(len(data))
+users.append("Elsa")
+print(users)
+
+users += ["Sason"]
+print(users)
+
+users.extend(["Robert", "Jimy"])
+print(users)
+
+# users.extend(data)
+# print(users)
+
+users.insert(0, "Bob")
+print(users)
+
+users[2:2] = ["Eddie", "Alex"]
+print(users)
+
+users[1:3] = ["Robert", "JPJ"]
+print(users)
+
+users.remove("Bob")
+print(users)
+print(users.pop())
+print(users)
+
+del users[0]
+print(users)
+
+# del data
+data.clear()
+print(data)
+
+users[1:2] = ["Dave"]
+users.sort()
+print(users)
+users.sort(key=str.lower)
+print(users)
