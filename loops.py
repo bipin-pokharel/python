00262de4c8adcbf930d3cde471384e3ebbf3358c
bipin pokharel <pokharelbipin850@gmail.com>
2026-03-14 15:57:53 +0545

    loops start

---
+++ loops.py
@@ -0,0 +1,47 @@
+# value = 1
+# while value <= 10:
+#     print(value)
+#     if value == 5:
+#         break
+#     value += 1
+
+# value = 1
+# while value <= 10:
+#     value += 1
+#     if value == 5:
+#         continue
+#     print(value)
+# else:
+#     print("value is now equal to" + str(value))
+
+names = ["dave", "sara", "john"]
+# for x in names:
+#     print(x)
+
+# for x in "Mississippi":
+#     print(x)
+
+# for x in names:
+#     if x == "sara":
+#         break
+#     print(x)
+
+# for x in names:
+#     if x == "sara":
+#         continue
+#     print(x)
+
+# for x in range(4):
+#     print(x)
+
+for x in range(0, 101, 5):
+    print(x)
+else:
+    print("glad that's over")
+
+names = ["Dave", "Sara", "John"]
+actions = ["codes", "eats", "sleeps"]
+
+for name in names:
+    for action in actions:
+        print(name + "" + action + ".")
