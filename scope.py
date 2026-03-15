f18f6ec2cc761a74d10cc8752583bcb79acb7137
bipin pokharel <pokharelbipin850@gmail.com>
2026-03-15 22:41:34 +0545

    global

--- scope.py
+++ scope.py
@@ -1,10 +1,16 @@
 name = "dave"
+count = 1
 
 
 def another():
     color = "blue"
+    global count
+    count += 1
+    print(count)
 
     def greeting(name):
+        nonlocal color
+        color = "red"
         print(color)
         print(name)
 
