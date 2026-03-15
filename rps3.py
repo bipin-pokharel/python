a43bc645aa65d15abd6b086c327e1707c60a311d
bipin pokharel <pokharelbipin850@gmail.com>
2026-03-15 22:13:24 +0545

    recursion in game

---
+++ rps3.py
@@ -0,0 +1,55 @@
+import sys
+import random
+from enum import Enum
+
+
+def play_rps():
+
+    class RPS(Enum):
+        ROCK = 1
+        PAPER = 2
+        SCISSORS = 3
+
+    playerchoice = input("\nEnter.. \n1 For Rock, \n2 For Paper, \n3 For Scissors :\n")
+
+    if playerchoice not in ["1", "2", "3"]:
+        print("You must enter 1 , 2 , 3")
+        return play_rps()
+
+    player = int(playerchoice)
+
+    computerchoice = random.choice("123")
+    computer = int(computerchoice)
+
+    print("\nYou chose " + str(RPS(player)).replace("RPS.", "") + ".")
+    print("Python chose " + str(RPS(computer)).replace("RPS.", "") + ".\n")
+    print("")
+
+    if player == 1 and computer == 3:
+        print("🎉 you won!")
+    elif player == 2 and computer == 1:
+        print("🎉 you won!")
+    elif player == 3 and computer == 2:
+        print(" 🎉you won!")
+    elif player == computer:
+        print("🫨 Tie game")
+    else:
+        print("🐍python wins!")
+
+    print("\n playagain?")
+
+    while True:
+        playagain = input("\nPlay again? \nY for YES or \nQ for Quit\n")
+        if playagain.lower() not in ["y", "q"]:
+            continue
+        else:
+            break
+        if playagain.lower() == "y":
+            return play_rps()
+        else:
+            print("\nCelebrate")
+            print("thank you for playing")
+            sys.exit("bye")
+
+
+play_rps()
