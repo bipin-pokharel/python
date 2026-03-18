1c1ea68bc0e15f5c705c7a59f9a9d8aca5730585
bipin pokharel <pokharelbipin850@gmail.com>
2026-03-18 15:57:54 +0545

    f strings

--- f_strings.py
+++ f_strings.py
@@ -1,4 +1,4 @@
-person = "dave"
+person = "Dave"
 coins = 3
 
 print("\n" + person + "has" + str(coins) + "coin left.")
@@ -17,3 +17,27 @@ print(message)
 
 player = {"person": "dave", "coins": 3}
 message = "\n{person}  has {coins}  coins  left.".format(**player)
+print(message)
+
+# f strings This is the way
+message = f"\n{person} has {coins} coins left."
+print(message)
+
+message = f"\n{person} has {2 * 5} coins left."
+print(message)
+
+message = f"\n{person.lower()} has {2 * 5} coins left."
+print(message)
+
+message = f"\n{player['person']} has {2 * 5} coins left."
+print(message)
+
+# You can pass formatting options
+num = 10
+print(f"\n2.25 times {num} is {2.25*num:.2f}\n")
+
+for num in range(1, 11):
+    print(f"2.25 times {num} is {2.25*num:.2f}")
+
+for num in range(1, 11):
+    print(f"{num} divided by 4.52 is{num/4.52:.2%}")
