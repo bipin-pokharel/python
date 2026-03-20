08f7a2e8ee06aab7e846d86a043774a52cfec865
bipin pokharel <pokharelbipin850@gmail.com>
2026-03-20 19:57:04 +0545

    personalized and rps8

--- personalized_person.py
+++ personalized_person.py
@@ -1,20 +1,20 @@
 def hello(name, lang):
-    greetings = {"English:Hello" "Spanish:Hola" "German:hallo"}
+    greetings = {"English": "Hello", "Spanish": "Hola", "German": "hallo"}
 
-    msg = f"{greetings[lang]}{name}"
+    msg = f"{greetings[lang]},{name}!"
     print(msg)
 
 
 if __name__ == "__main__":
     import argparse
 
-    parser = argparse.ArgumentParser(description="provides personal greeting.")
+    parser = argparse.ArgumentParser(description="Provides personal greeting.")
     parser.add_argument(
         "-n",
         "--name",
         metavar="name",
         required=True,
-        help="the name of the person is greet",
+        help="The name of the person is greet",
     )
 
     parser.add_argument(
@@ -23,7 +23,7 @@ if __name__ == "__main__":
         metavar="language",
         required=True,
         choices=["English", "Spanish", "German"],
-        help="THe language of the greeting.",
+        help="The language of the greeting.",
     )
 
     args = parser.parse_args()
